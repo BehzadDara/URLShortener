72e9242429de7f35d00fa842949d3b8ad1caf2b9
behzad dara <Behzad.dara.99@gmail.com>
2024-09-11 00:03:13 +0330

    load tests added

---
+++ load.js
@@ -0,0 +1,22 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '10s', target: 10000 },
+        { duration: '10s', target: 10000 },
+        { duration: '10s', target: 0 }, 
+    ],
+};
+
+export default function () {
+    const url = 'http://localhost:5249/urls/test';
+
+    let response = http.get(url);
+
+    check(response, {
+        'status is redirect': (r) => r.status === 301
+    });
+
+    sleep(1);
+}
---
+++ stress.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options = {
+    stages: [
+        { duration: '5s', target: 5000 }, 
+        { duration: '5s', target: 10000 }, 
+        { duration: '5s', target: 15000 },
+        { duration: '5s', target: 15000 }, 
+        { duration: '5s', target: 0 },
+    ],
+};
+
+export default function () {
+    const url = 'http://localhost:5249/urls/test';
+
+    let response = http.get(url);
+
+    check(response, {
+        'status is redirect': (r) => r.status === 301,
+    });
+
+    sleep(1);
+}
