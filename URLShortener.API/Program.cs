dce8d026c55e046ba0f2c68a5648c71876794ced
behzad dara <Behzad.dara.99@gmail.com>
2024-09-13 14:59:05 +0330

    dark theme v1

--- URLShortener.API/Program.cs
+++ URLShortener.API/Program.cs
@@ -75,19 +75,14 @@ var localizationOptions = new RequestLocalizationOptions()
 
 app.UseRequestLocalization(localizationOptions);
 
-//app.UseStaticFiles();
+app.UseStaticFiles();
 
 if (app.Environment.IsDevelopment())
 {
     var env = app.Services.GetRequiredService<IWebHostEnvironment>();
 
     app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "URL Shortener");
-        //c.InjectStylesheet("/swagger-ui/Custom.css");
-        c.IndexStream = () => File.OpenRead(Path.Combine(env.ContentRootPath, "wwwroot/swagger/index.html"));
-    });
+    app.UseSwaggerUI();
 }
 
 app.UseMiddleware<GlobalExceptionHandler>();
